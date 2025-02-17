c5d3bf6995e7cf7bfdf570bb6cff233f55ab5030
kristiansitinjak <kristiansitinjak270@gmail.com>
2025-02-17 14:25:17 +0700

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,58 +38,63 @@
         <!-- Spinner End -->
 
 
-        <!-- Navbar start -->
-        <div class="container-fluid fixed-top px-0">
-            <div class="container px-0">
-                <div class="topbar">
-                    <div class="row align-items-center justify-content-center">
-                        <div class="col-md-8">
-                        </div>
-                        <div class="col-md-4">
-                            <div class="topbar-icon d-flex align-items-center justify-content-end">
-                                <a href="#" class="btn-square text-white me-2"><i class="fab fa-facebook-f"></i></a>
-                                <a href="#" class="btn-square text-white me-2"><i class="fab fa-twitter"></i></a>
-                                <a href="#" class="btn-square text-white me-2"><i class="fab fa-instagram"></i></a>
-                                <a href="#" class="btn-square text-white me-2"><i class="fab fa-pinterest"></i></a>
-                                <a href="#" class="btn-square text-white me-0"><i class="fab fa-linkedin-in"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <nav class="navbar navbar-light bg-light navbar-expand-xl">
-                    <a href="index.html" class="navbar-brand ms-3">
-                        <h1 class="text-primary display-5">HIMATIF</h1>
-                    </a>
-                    <button class="navbar-toggler py-2 px-3 me-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-                        <span class="fa fa-bars text-primary"></span>
-                    </button>
-                    <div class="collapse navbar-collapse bg-light" id="navbarCollapse">
-                        <div class="navbar-nav ms-auto">
-                            <a href="index.html" class="nav-item nav-link active">Home</a>
-                            <a href="about.html" class="nav-item nav-link">About</a>
-                            <a href="service.html" class="nav-item nav-link">Services</a>
-                            <a href="causes.html" class="nav-item nav-link">Causes</a>
-                            <a href="events.html" class="nav-item nav-link">Events</a>
-                            <div class="nav-item dropdown">
-                                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-                                <div class="dropdown-menu m-0 bg-secondary rounded-0">
-                                    <a href="blog.html" class="dropdown-item">Blog</a>
-                                    <a href="gallery.html" class="dropdown-item">Gallery</a>
-                                    <a href="volunteer.html" class="dropdown-item">Volunteers</a>
-                                    <a href="donation.html" class="dropdown-item">Donation</a>
-                                    <a href="404.html" class="dropdown-item">404 Error</a>
+            <!-- Navbar Start -->
+            <div class="container-fluid fixed-top px-0">
+                <div class="container px-0">
+                    <div class="topbar">
+                        <div class="row align-items-center justify-content-center">
+                            <div class="col-md-8"></div>
+                            <div class="col-md-4">
+                                <div class="topbar-icon d-flex align-items-center justify-content-end">
+                                    <a href="#" class="btn-square text-white me-2"><i class="fab fa-facebook-f"></i></a>
+                                    <a href="#" class="btn-square text-white me-2"><i class="fab fa-twitter"></i></a>
+                                    <a href="#" class="btn-square text-white me-2"><i class="fab fa-instagram"></i></a>
+                                    <a href="#" class="btn-square text-white me-2"><i class="fab fa-pinterest"></i></a>
+                                    <a href="#" class="btn-square text-white me-0"><i class="fab fa-linkedin-in"></i></a>
                                 </div>
                             </div>
-                            <a href="contact.html" class="nav-item nav-link">Contact</a>
-                        </div>
-                        <div class="d-flex align-items-center flex-nowrap pt-xl-0" style="margin-left: 15px;">
-                            <a href="" class="btn-hover-bg btn btn-primary text-white py-2 px-4 me-3">Donate Now</a>
                         </div>
                     </div>
-                </nav>
+
+                    <nav class="navbar navbar-light bg-light navbar-expand-xl">
+                        <a href="{{ route('home') }}" class="navbar-brand ms-3">
+                            <h1 class="text-primary display-5">HIMATIF</h1>
+                        </a>
+                        <button class="navbar-toggler py-2 px-3 me-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                            <span class="fa fa-bars text-primary"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse bg-light" id="navbarCollapse">
+                            <div class="navbar-nav ms-auto">
+                                <a href="{{ route('home') }}" class="nav-item nav-link {{ request()->is('/') ? 'active' : '' }}">Home</a>
+                                <a href="{{ route('about') }}" class="nav-item nav-link {{ request()->is('about') ? 'active' : '' }}">About</a>
+                                <a href="{{ route('service') }}" class="nav-item nav-link {{ request()->is('service') ? 'active' : '' }}">Services</a>
+                                <a href="{{ route('causes') }}" class="nav-item nav-link {{ request()->is('causes') ? 'active' : '' }}">Causes</a>
+                                <a href="{{ route('events') }}" class="nav-item nav-link {{ request()->is('events') ? 'active' : '' }}">Events</a>
+
+                                <div class="nav-item dropdown">
+                                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                                    <div class="dropdown-menu m-0 bg-secondary rounded-0">
+                                        <a href="{{ route('blog') }}" class="dropdown-item">Blog</a>
+                                        <a href="{{ route('gallery') }}" class="dropdown-item">Gallery</a>
+                                        <a href="{{ route('volunteer') }}" class="dropdown-item">Volunteers</a>
+                                        <a href="{{ route('donation') }}" class="dropdown-item">Donation</a>
+                                        <a href="{{ route('error404') }}" class="dropdown-item">404 Error</a>
+                                    </div>
+                                </div>
+
+                                <a href="{{ route('contact') }}" class="nav-item nav-link {{ request()->is('contact') ? 'active' : '' }}">Contact</a>
+                            </div>
+
+                            <div class="d-flex align-items-center flex-nowrap pt-xl-0" style="margin-left: 15px;">
+                                <a href="#" class="btn-hover-bg btn btn-primary text-white py-2 px-4 me-3">Donate Now</a>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
             </div>
-        </div>
-        <!-- Navbar End -->
+            <!-- Navbar End -->
+
 
         
        <!-- Carousel Start -->
@@ -217,7 +222,7 @@
 
 
         <!-- Services Start -->
-        <div class="container-fluid service py-5 bg-light">
+        <!-- <div class="container-fluid service py-5 bg-light">
             <div class="container py-5">
                 <div class="text-center mx-auto pb-5" style="max-width: 800px;">
                     <h5 class="text-uppercase text-primary">What we do</h5>
@@ -271,7 +276,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
         <!-- Services End -->
 
 
@@ -279,8 +284,8 @@
         <div class="container-fluid donation py-5">
             <div class="container py-5">
                 <div class="text-center mx-auto pb-5" style="max-width: 800px;">
-                    <h5 class="text-uppercase text-primary">Donation</h5>
-                    <h1 class="mb-0">Your money will save our life</h1>
+                    <h5 class="text-uppercase text-primary">SEPUTAR HIMATIF IINSTITUT TEKNOLOLGI DEL</h5>
+                    <h1 class="mb-3">Dapatkan Berita Terbaru Himatif IT Del</h1>
                 </div>
                 <div class="row g-4">
                     <div class="col-lg-4">
--- routes/web.php
+++ routes/web.php
@@ -3,49 +3,45 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/404', function () {
-    return view('404');
-});
+    return view('welcome'); // Halaman utama
+})->name('home');
 
 Route::get('/about', function () {
     return view('about');
-});
-
-Route::get('/blog', function () {
-    return view('blog');
-});
-
-Route::get('/causes', function () {
-    return view('causes');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-Route::get('/donation', function () {
-    return view('donation');
-});
-
-Route::get('/events', function () {
-    return view('events');
-});
-
-Route::get('/gallery', function () {
-    return view('gallery');
-});
-
-Route::get('/index', function () {
-    return view('index');
-});
+})->name('about');
 
 Route::get('/service', function () {
     return view('service');
-});
+})->name('service');
+
+Route::get('/causes', function () {
+    return view('causes');
+})->name('causes');
+
+Route::get('/events', function () {
+    return view('events');
+})->name('events');
+
+Route::get('/blog', function () {
+    return view('blog');
+})->name('blog');
+
+Route::get('/gallery', function () {
+    return view('gallery');
+})->name('gallery');
 
 Route::get('/volunteer', function () {
     return view('volunteer');
-});
+})->name('volunteer');
+
+Route::get('/donation', function () {
+    return view('donation');
+})->name('donation');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/404', function () {
+    return view('404');
+})->name('error404');

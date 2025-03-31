9829e1e3bb6a2ba49acba680e51e91c1dc7fd3ac
kristiansitinjak <kristiansitinjak270@gmail.com>
2025-03-31 21:50:01 +0700

    aan

--- routes/web.php
+++ routes/web.php
@@ -178,6 +178,7 @@ Route::get('/admin2/proposals/rejected', [ProposalController::class, 'rejectedAd
 // Rute LOGIN
 // ============================
 
+
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
